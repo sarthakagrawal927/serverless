3b865c9e5827b22d885a04eff480310aea5ee49d
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2023-04-18 01:01:08 +0530

    Add basic s3 upload logic

---
+++ s3-upload/go.mod
@@ -0,0 +1,10 @@
+module github.com/sarthakagrawal927/serverless/s3-upload
+
+go 1.20
+
+require github.com/aws/aws-sdk-go v1.44.245
+
+require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
---
+++ s3-upload/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awsutil"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	godotenv.Load()
+}
+
+func main() {
+	aws_access_key_id := os.Getenv("AWS_ACCESS_KEY_ID")
+	aws_secret_access_key := os.Getenv("AWS_SECRET_ACCESS_KEY")
+	token := os.Getenv("AWS_ACCESS_TOKEN")
+	creds := credentials.NewStaticCredentials(aws_access_key_id, aws_secret_access_key, token)
+	_, err := creds.Get()
+	if err != nil {
+		// handle error
+	}
+	cfg := aws.NewConfig().WithRegion("ap-south-1").WithCredentials(creds)
+	svc := s3.New(session.New(), cfg)
+
+	file, err := os.Open("test.png")
+	if err != nil {
+		// handle error
+	}
+	defer file.Close()
+	fileInfo, _ := file.Stat()
+	size := fileInfo.Size()
+	buffer := make([]byte, size) // read file content to buffer
+
+	file.Read(buffer)
+	fileBytes := bytes.NewReader(buffer)
+	fileType := http.DetectContentType(buffer)
+	path := "/media/" + file.Name()
+	params := &s3.PutObjectInput{
+		Bucket:        aws.String("stumblers"),
+		Key:           aws.String(path),
+		Body:          fileBytes,
+		ContentLength: aws.Int64(size),
+		ContentType:   aws.String(fileType),
+	}
+	resp, err := svc.PutObject(params)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(awsutil.Prettify(resp))
+}
